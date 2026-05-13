338110083750769ae2671d2f44a046059da4763e
Sergei Privalov <privalov.s.a89@gmail.com>
2026-05-13 10:07:16 -0500

    feat: Black mode
    Agent: JohnFrontendMcClane
    Task-Id: ee8a9431-8a02-42f3-a57d-4cd4e1631314

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
+import { useEffect } from 'react'
 import { useSelector } from 'react-redux'
+import { ConfigProvider, theme as antTheme } from 'antd'
 import type { RootState } from './store'
 import LoginPage from './pages/LoginPage'
 import DashboardPage from './pages/DashboardPage'
 
 export default function App() {
   const accessToken = useSelector((state: RootState) => state.auth.accessToken)
-  return accessToken ? <DashboardPage /> : <LoginPage />
+  const isDark = useSelector((state: RootState) => state.theme.isDark)
+
+  useEffect(() => {
+    document.documentElement.classList.toggle('dark', isDark)
+  }, [isDark])
+
+  return (
+    <ConfigProvider
+      theme={{ algorithm: isDark ? antTheme.darkAlgorithm : antTheme.defaultAlgorithm }}
+    >
+      {accessToken ? <DashboardPage /> : <LoginPage />}
+    </ConfigProvider>
+  )
 }
